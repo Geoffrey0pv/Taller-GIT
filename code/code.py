f24815e23a364998dd5bf94f97a767319af3d258
Juan José Angarita Yela <129020936+RamonaFlores@users.noreply.github.com>
2024-09-09 17:05:56 -0500

    feature: creación clase main + funcionalidades
    Implementa la clase "Main" que posea una lista de vehículos y permita agregarlos y
    buscarlos por año.

---
+++ code/code.py
@@ -0,0 +1,31 @@
+
+class Vehiculo:
+    VALID_FUEL_TYPES = ['Gasolina', 'Diesel', 'Eléctrico']
+
+    def _init_(self, marca, modelo, año, kilometraje, estado_actual, tipo_combustible, potencia):
+        if tipo_combustible not in Vehiculo.VALID_FUEL_TYPES:
+            raise ValueError(f"Tipo de combustible no válido. Debe ser uno de {Vehiculo.VALID_FUEL_TYPES}")
+        self._marca = marca
+        self._modelo = modelo
+        self._año = año
+        self._kilometraje = kilometraje
+        self._estado_actual = estado_actual
+        self._tipo_combustible = tipo_combustible
+        self._potencia = potencia
+
+    # Getter and Setter for potencia
+    def get_potencia(self):
+        return self._potencia
+
+    def set_potencia(self, potencia):
+        self._potencia = potencia
+
+class Main:
+    def _init_(self):
+        self.vehiculos = []
+
+    def agregar_vehiculo(self, vehiculo):
+        self.vehiculos.append(vehiculo)
+
+    def buscar_vehiculo_por_año(self, año):
+        return [v for v in self.vehiculos if v.get_año() == año]

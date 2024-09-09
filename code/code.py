05a374bfc189a6bb416e67db570eb0a51a983087
Juan Camilo Tobar <tobarjuanca9@gmail.com>
2024-09-09 16:57:32 -0500

    fix: Se fixeo el codigo de creacion de vehiculos

--- code/code.py
+++ code/code.py
@@ -1,45 +1,47 @@
-def _init_(self, marca, modelo, año, kilometraje, estado_actual, tipo_combustible):
-self._marca = marca
-self._modelo = modelo
-self._año = año
-self._kilometraje = kilometraje
-self._estado_actual = estado_actual
-self._tipo_combustible = tipo_combustible
+class Vehiculo:
+    def __init__(self, marca, modelo, año, kilometraje, estado_actual, tipo_combustible):
+        self._marca = marca
+        self._modelo = modelo
+        self._año = año
+        self._kilometraje = kilometraje
+        self._estado_actual = estado_actual
+        self._tipo_combustible = tipo_combustible
 
-# Getters
-def get_marca(self):
-return self._marca
+    # Getters
+    def get_marca(self):
+        return self._marca
 
-def get_modelo(self):
-return self._modelo
+    def get_modelo(self):
+        return self._modelo
 
-def get_año(self):
-return self._año
+    def get_año(self):
+        return self._año
 
-def get_kilometraje(self):
-return self._kilometraje
+    def get_kilometraje(self):
+        return self._kilometraje
 
-def get_estado_actual(self):
-return self._estado_actual
+    def get_estado_actual(self):
+        return self._estado_actual
 
-def get_tipo_combustible(self):
-return self._tipo_combustible
+    def get_tipo_combustible(self):
+        return self._tipo_combustible
 
-# Setters
-def set_marca(self, marca):
-self._marca = marca
+    # Setters
+    def set_marca(self, marca):
+        self._marca = marca
 
-def set_modelo(self, modelo):
-self._modelo = modelo
+    def set_modelo(self, modelo):
+        self._modelo = modelo
 
-def set_año(self, año):
-self._año = año
+    def set_año(self, año):
+        self._año = año
 
-def set_kilometraje(self, kilometraje):
-self._kilometraje = kilometraje
+    def set_kilometraje(self, kilometraje):
+        self._kilometraje = kilometraje
 
-def set_estado_actual(self, estado_actual):
-self._estado_actual = estado_actual
+    def set_estado_actual(self, estado_actual):
+        self._estado_actual = estado_actual
 
-def set_tipo_combustible(self, tipo_combustible):
-self._tipo_combustible = tipo_combustible
+    def set_tipo_combustible(self, tipo_combustible):
+        self._tipo_combustible = tipo_combustible
+``
